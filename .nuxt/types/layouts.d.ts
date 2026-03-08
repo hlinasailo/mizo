92ed2ce7357febdb5903e0a5217a5835240e76c2
yuzuhisa <dinzd155@gmail.com>
2026-03-08 18:09:52 +0530

    Add .data, .nuxt directories and apiSchema.yml

---
+++ .nuxt/types/layouts.d.ts
@@ -0,0 +1,16 @@
+import type { ComputedRef, MaybeRef } from 'vue'
+
+type ComponentProps<T> = T extends new(...args: any) => { $props: infer P } ? NonNullable<P>
+  : T extends (props: infer P, ...args: any) => any ? P
+  : {}
+
+declare module 'nuxt/app' {
+  interface NuxtLayouts {
+    default: ComponentProps<typeof import("D:/mizomade frontend/mizomade_frontend_v2/frontend/app/layouts/default.vue").default>,
+    navbar: ComponentProps<typeof import("D:/mizomade frontend/mizomade_frontend_v2/frontend/app/layouts/navbar.vue").default>,
+}
+  export type LayoutKey = keyof NuxtLayouts extends never ? string : keyof NuxtLayouts
+  interface PageMeta {
+    layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
+  }
+}
